04b86fda24d8dcd7d74495e586ebabd6293bc843
Brittany <leeabrittany@gmail.com>
2014-04-15 11:24:40 -0500

    print most recent changes

--- data.py
+++ data.py
@@ -1,6 +1,10 @@
 import urllib2, json
 from pprint import pprint
 
+
+print "Most Recent Changes:"
+print
+
 url ="https://en.wikipedia.org/w/api.php?format=json&action=query&list=recentchanges"
 try:
 	result = urllib2.urlopen(url)
@@ -11,25 +15,20 @@ except urllib2.URLError, e:
 
 data = json_data[u'query'][u'recentchanges']
 
-# for row in data:
-# 	nquery = "https://en.wikipedia.org/w/api.php?action=query&pageids=" + str(row[u'pageid']) + "&prop=info&inprop=url"
-# 	print nquery
-# 	result = urllib2.urlopen(nquery)
-# 	json_data = json.loads(result.read())
-# 	print pprint(json_data)
+for row in data:
+	pageid = row[u'pageid']
+	nquery = "https://en.wikipedia.org/w/api.php?format=json&action=query&pageids=" + str(pageid) + "&prop=info&inprop=url"
 
-row = data[0]
-nquery = "https://en.wikipedia.org/w/api.php?format=json&action=query&pageids=" + str(row[u'pageid']) + "&prop=info&inprop=url"
-print nquery
-print
-pageid = row[u'pageid']
-print pageid
-try:
-	result = urllib2.urlopen(nquery)
-	json_data = json.loads(result.read())
-	# print pprint(json_data)
-except urllib2.URLError, e:
-	handleError(e)
+	try:
+		result = urllib2.urlopen(nquery)
+		json_data = json.loads(result.read())
+		# print pprint(json_data)
+	except urllib2.URLError, e:
+		handleError(e)
 
-data = json_data[u'query'][u'pages'][str(pageid)][u'fullurl']
-print data
+	meta_data = json_data[u'query'][u'pages'][str(pageid)]
+	name = meta_data[u'title']
+	url = meta_data[u'fullurl']
+	print name
+	print url
+	print
