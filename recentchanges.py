33875eb019bdb2f46ba76cb56967d4da4306d1f2
Brittany <leeabrittany@gmail.com>
2014-04-21 17:42:32 -0500

    add rising weekly data

--- recentchanges.py
+++ recentchanges.py
@@ -1,69 +1,59 @@
 import urllib2, json
 from pprint import pprint
 
+def get_json(url):
+	try:
+		result = urllib2.urlopen(url)
+		json_data = json.loads(result.read())
+		# print pprint(json_data)
+	except urllib2.URLError, e:
+		handleError(e)
+	return json_data
 
-user_agent = 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_4; en-US) AppleWebKit/534.3 (KHTML, like Gecko) Chrome/6.0.472.63 Safari/534.3'
-headers = { 'User-Agent' : user_agent }
-req = urllib2.Request('http://en.wikipedia.org/wiki/Wikipedia:Top25Report', None, headers)
-response = urllib2.urlopen(req)
-page = response.read()
-print page
+def get_rowdata(pageid):
+	nquery = "https://en.wikipedia.org/w/api.php?format=json&action=query&pageids=" + str(pageid) + "&prop=info&inprop=url"
 
+	try:
+		result = urllib2.urlopen(nquery)
+		json_data = json.loads(result.read())
+		# print pprint(json_data)
+	except urllib2.URLError, e:
+		handleError(e)
 
+	meta_data = json_data[u'query'][u'pages'][str(pageid)]
+	name = meta_data[u'title']
+	url = meta_data[u'fullurl']
+	return name, url
 
-# def get_json(url):
-# 	try:
-# 		result = urllib2.urlopen(url)
-# 		json_data = json.loads(result.read())
-# 		# print pprint(json_data)
-# 	except urllib2.URLError, e:
-# 		handleError(e)
-# 	return json_data
+def get_mostrecent():
+	url = "https://en.wikipedia.org/w/api.php?format=json&action=query&list=recentchanges"
+	json_data = get_json(url)
+	data = json_data[u'query'][u'recentchanges']
 
-# def get_rowdata(pageid):
-# 	nquery = "https://en.wikipedia.org/w/api.php?format=json&action=query&pageids=" + str(pageid) + "&prop=info&inprop=url"
+	print "Most Recent Changes"
+	for row in data:
+		pageid = row[u'pageid']
+		name, url = get_rowdata(pageid)
+		print name
+		print url
+		print
 
-# 	try:
-# 		result = urllib2.urlopen(nquery)
-# 		json_data = json.loads(result.read())
-# 		# print pprint(json_data)
-# 	except urllib2.URLError, e:
-# 		handleError(e)
+def get_nearme():
+	# uses longitude and latitude for Evanston
+	url = "http://en.wikipedia.org/w/api.php?format=json&action=query&list=geosearch&gsradius=10000&gscoord=42.0586|-87.6845"
+	json_data = get_json(url)
+	data = json_data[u'query'][u'geosearch']
 
-# 	meta_data = json_data[u'query'][u'pages'][str(pageid)]
-# 	name = meta_data[u'title']
-# 	url = meta_data[u'fullurl']
-# 	return name, url
+	print "Articles Near Me"
+	for row in data:
+		pageid = row[u'pageid']
+		name, url = get_rowdata(pageid)
+		print name
+		print url
+		print
 
-# def get_mostrecent():
-# 	url = "https://en.wikipedia.org/w/api.php?format=json&action=query&list=recentchanges"
-# 	json_data = get_json(url)
-# 	data = json_data[u'query'][u'recentchanges']
-
-# 	print "Most Recent Changes"
-# 	for row in data:
-# 		pageid = row[u'pageid']
-# 		name, url = get_rowdata(pageid)
-# 		print name
-# 		print url
-# 		print
-
-# def get_nearme():
-# 	# uses longitude and latitude for Evanston
-# 	url = "http://en.wikipedia.org/w/api.php?format=json&action=query&list=geosearch&gsradius=10000&gscoord=42.0586|-87.6845"
-# 	json_data = get_json(url)
-# 	data = json_data[u'query'][u'geosearch']
-
-# 	print "Articles Near Me"
-# 	for row in data:
-# 		pageid = row[u'pageid']
-# 		name, url = get_rowdata(pageid)
-# 		print name
-# 		print url
-# 		print
-
-# get_mostrecent()
-# get_nearme()
+get_mostrecent()
+get_nearme()
 
 # print "Most Recent Changes:"
 # print
