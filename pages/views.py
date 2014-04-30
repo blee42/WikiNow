858938cb4960d454eb0bba7316b0199dc4c9e483
kklai <kkrebeccalai@gmail.com>
2014-04-30 12:26:30 -0500

    add homepage

--- pages/views.py
+++ pages/views.py
@@ -4,12 +4,9 @@ import grab_top25, grab_current, risingweekly, risingdaily
 #import sys.path.append('../wikinow/grab_content/grab_current.py')
 
 def pages(request):
-	#pages = Page.objects.all()
-	name = "hello world!"
-	return render(request, "pages.html", locals())
+	return render(request, "index.html", locals())
 
 def test(request):
-	name = "hello world!"
 	headline, previous, summary, content = grab_top25.grab_25()
 	return render(request, "test.html", locals())
 
