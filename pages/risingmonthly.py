33db4bbf0a2f33628c929a923e16724596e31597
Brittany <leeabrittany@gmail.com>
2014-05-12 19:33:48 -0500

    add google images to daily and weekly

--- pages/risingmonthly.py
+++ pages/risingmonthly.py
@@ -36,19 +36,6 @@ def month():
 		img_url = img_data[u'url']
 
 		result['img'] = img_url
-		# img_soup2 = img_soup.find('table', {'class': 'infobox'})
-		# if (img_soup2):
-		# 	img_soup3 = img_soup2.find_all('img')
-		# 	if (len(img_soup3) != 0):
-		# 		img_link = img_soup3[0].get('src')
-		# 		result['img'] = 'http://' + img_link[2:]
-		# 	else:
-		# 		# print "No image in info box"
-		# 		result['img'] = "No image available"
-		# else:
-		# 	# print "No info box"
-		# 	img_link = img_soup.find_all('img')[0].get('src')
-		# 	result['img'] = 'http://' + img_link[2:]
 		
 		#print 'rank: ' + str(count)
 		result['ranks'] = str(count)
