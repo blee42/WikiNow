941e452761054c91c620b78bcb6caecdfb79170c
Brittany <leeabrittany@gmail.com>
2014-05-28 19:53:27 -0500

    add static to urls

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.views.generic import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
 
 admin.autodiscover()
 
@@ -10,7 +12,6 @@ urlpatterns = patterns('',
     # url(r'^{{ project_name }}/', include('{{ project_name }}.foo.urls')),
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     # url(r'^$', TemplateView.as_view(template_name='index.html')),
-    url(r'^static/$', 'django.views.static.serve', {'document_root':'settings.STATIC_ROOT'}),
     url(r'^$', 'pages.views.home', name='home'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', 'pages.views.pages', name='pages'),
@@ -18,4 +19,4 @@ urlpatterns = patterns('',
     url(r'^pages/', 'pages.views.pages', name='pages'),
     url(r'^daily/', 'pages.views.daily', name='daily'),
     url(r'^monthly/', 'pages.views.monthly', name='monthly')
-)
+) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
